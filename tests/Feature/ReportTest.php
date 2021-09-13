26eb953172c56116e795d862743949f5d14eb44c
Hirokazu Okawa <h.okyawa@gmail.com>
2021-09-14 05:31:31 +0900

    11-2-2: 最初のテスト

---
+++ tests/Feature/ReportTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+/**
+ * このファイルの生成コマンド
+ * $ php artisan make:test ReportTest
+ */
+class ReportTest extends TestCase
+{
+    /**
+     * リスト 11.2.2.3
+     * Todoリスト項目をそのままメソッド名として、コード例に示すメソッドを追加
+     *
+     * @test
+     */
+    public function api_customerにGETリクエストでアクセスできる(): void
+    {
+    }
+}
